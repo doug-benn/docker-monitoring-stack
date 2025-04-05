40b0f775033fc632d6c62f8b96e962ecf29f0ba7
doug-benn <fiery.coder@gmail.com>
2025-04-05 13:46:57 +0100

    added system overview

--- demo-app/app.py
+++ demo-app/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import time 
+import sys
 
 app = Flask(__name__)
 
@@ -8,4 +10,6 @@ def hello():
 
 
 if __name__ == "__main__":
+    time.sleep(10)
+    sys.exit()
     app.run(host='0.0.0.0', port=8000)
